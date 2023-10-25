2ff57761da5355b5eb7a89361f9779b7678fd805
lalabib <labibla21@gmail.com>
2023-10-26 05:45:28 +0700

    remove unused import

--- app/src/main/java/com/lalabib/mamamapp/ui/favorite/FavoriteActivity.kt
+++ app/src/main/java/com/lalabib/mamamapp/ui/favorite/FavoriteActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.lalabib.mamamapp.R
